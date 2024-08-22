7c585731d408abd70302e562a8c1282cfd02a660
Giovanni Gonçalves <giovanni.ggoncalves@outlook.com>
2024-08-22 17:15:07 -0300

    feat: criação do servidor

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { config } from 'dotenv'
+
+config ()
+
+const serverPort = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    return res.status(200).send({ message: "Hello, World!"})
+})
+
+app.listen(serverPort, () => {
+    console.log(`😍 Server started on http://localhost:${serverPort}`)
+})
