89e336d0b44c118ba4c2e90f840163a6aa9d249f
Giovanni Gonçalves <giovanni.ggoncalves@outlook.com>
2024-08-29 10:53:30 -0300

    feat: add some request

--- src/server.js
+++ src/server.js
@@ -9,11 +9,54 @@ const app = express()
 
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Alegria",
+        cor: "Azul"
+    },
+    {
+        id: 2,
+        nome: "Tristeza",
+        cor: "Roxo"
+    },
+    {
+        id: 3,
+        nome: "Nojinho",
+        cor: "Verde"
+    }
+
+]
+
+const personagens = [
+    {
+        id: 1,
+        nome: "goku",
+        vivo: true
+    },
+    {
+        id: 2,
+        nome: "vegeta",
+        vivo: true
+    },
+    {
+        id: 3,
+        nome: "freeza",
+        vivo: false
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
 })
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send( emocoes )
+})
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
+})
+app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens)
 })
 
 app.listen(serverPort, () => {
