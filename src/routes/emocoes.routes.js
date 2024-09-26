a50fe187d62ff334deece3c2561b1cb541bec06c
Giovanni <158181635+giovannig10@users.noreply.github.com>
2024-09-26 14:54:32 -0300

    feat: create update emotin

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,11 +20,11 @@ let emocoes = [
     }
 
 ]
-
+// rota para buscar todas as emoções
 emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send( emocoes )
 })
-
+// rota para criar uma nova emoção
 emocoesRoutes.post("/", (req, res) => {
     const {nome, cor} = req.body
     const novaEmocao = {
@@ -34,6 +34,45 @@ emocoesRoutes.post("/", (req, res) => {
     }
     emocoes.push(novaEmocao)
     return res.status(201).send( emocoes )
+})
+
+emocoesRoutes.get("/:id", (req, res) => {
+    const {id} = req.params
+    //console.log(id)
+    const emocao = emocoes.find((emotion) => emotion.id == id )
+
+    if (!emocao) {
+        return res.status(200).send ({
+            message: "Emoção não encontrada!", 
+        })}
+        return res.status(200).send({
+            message: "emocao encontrada",
+            emocao, 
+        })
+    })
+
+    emocoesRoutes.put("/:id", (req, res) => {
+        const {id} = req.params
+
+        const emocao = emocoes.find((emotion) => emotion.id == id )
+
+        if (!emocao) {
+            return res.status(200).send ({
+                message: "Emoção não encontrada!", 
+            })}
+            return res.status(200).send({
+                message: "emocao encontrada",
+                emocao, 
+            })
+            const {nome, cor} = req.body
+            emocao.nome = nome
+            emocao.cor = cor
+
+            return res.status(200).send({
+                message: "Emocao atualizada!",
+                emocao,
+        })
+
 })
 
 export default emocoesRoutes
