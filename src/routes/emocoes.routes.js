1d50ebee7a464f23d4dbf995c188c6820a29d773
Giovanni <158181635+giovannig10@users.noreply.github.com>
2024-09-26 15:18:08 -0300

    feat: create delete emotin method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -75,4 +75,21 @@ emocoesRoutes.get("/:id", (req, res) => {
 
 })
 
+emocoesRoutes.delete("/:id", (req, res) => {
+    const {id} = req.params
+
+    const emocao = emocoes.find((emotion) => emotion.id == id )
+
+    if (!emocao) {
+        return res.status(200).send ({
+            message: "Emoção não encontrada!", 
+        })}
+
+        emocoes = emocoes.filter((emotion) => emotion.id != id)
+        return res.status(200).send({
+            message: "emocao deletada",
+            emocao, 
+        })
+})
+
 export default emocoesRoutes
